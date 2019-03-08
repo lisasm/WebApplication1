6d6924647eed4ab8f480c5ab41e792fc3e2b7d6d
lisasm <lisa.smith@fox.com>
2019-03-07 18:05:49 -0800

    DbContext

--- Data.EF/AppDbContext.cs
+++ Data.EF/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using DomainModel;
+﻿using Data.EF.ConfigurationMappings;
+using DomainModel;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -8,8 +9,18 @@ namespace Data.EF
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
-//          => optionsBuilder.UseNpgsql("Host=localhost; Database=lisasm-webapp1;Username=postgres;Password=");
+ //         => optionsBuilder.UseNpgsql("Host=localhost; Database=lisasm-webapp1;Username=postgres;Password=");
 
         public DbSet<DomainModel.Program> Programs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ForNpgsqlUseIdentityColumns();
+
+            modelBuilder.ApplyConfiguration<Program>(new Program_Mapping());
+
+        }
     }
 }
---
+++ Data.EF/ConfigurationMappings/Program_Mapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.EF.ConfigurationMappings
+{
+    using DomainModel;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    internal class Program_Mapping : IEntityTypeConfiguration<Program>
+    {
+        public void Configure(EntityTypeBuilder<Program> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.ToTable("Programs");
+            builder.Property(t => t.Id).HasColumnName("Id").UseNpgsqlIdentityColumn();
+            builder.Property(t => t.Name).IsRequired().HasColumnName("Name").HasMaxLength(100);
+        }
+    }
+}
